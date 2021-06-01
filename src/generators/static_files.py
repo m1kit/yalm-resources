1cbcf627058f0a6f4a1b6a6e82580a0f237cea06
mikit <me@mikit.dev>
2021-06-02 01:39:26 +0900

    Fix unintentionally ignored file

--- src/generators/static_files.py
+++ src/generators/static_files.py
@@ -3,4 +3,4 @@ import shutil
 
 def generate(dest, meta):
   print(f"Copying static files...")
-  shutil.copytree('static/dist', dest, dirs_exist_ok=True)
+  shutil.copytree('static/raw', dest, dirs_exist_ok=True)
