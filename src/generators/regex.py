bf2c57830c3bfd521c5de01719ac1bb3be7f88b7
mikit <me@mikit.dev>
2021-06-24 22:31:17 +0900

    Normalize regexes

--- src/generators/regex.py
+++ src/generators/regex.py
@@ -4,7 +4,7 @@ from os.path import join as pjoin
 import json
 import progressbar
 import re
-from spdx_xml import template
+from spdx_xml import template, normalizer
 
 _dir = 'regex'
 
@@ -13,19 +13,23 @@ def generate(dest, meta):
   print(f"Generating regex...")
   with open(pjoin(dest, meta['licenses-file'])) as f:
     licenses = json.load(f)
+  with open(pjoin(dest, meta['equivalentwords-file'])) as f:
+    equivalentwords = json.load(f)
 
   os.makedirs(pjoin(dest, _dir), exist_ok=True)
 
+  nf = normalizer.TemplateNormalizer(equivalentwords)
   for license in progressbar.progressbar(licenses):
     license_id = license['id']
     regex_path = pjoin(dest, _dir, f"{license_id}")
     with open(pjoin(dest, meta['templates-dir'], f"{license_id}.json")) as f:
       root = template.parse_json(json.load(f))
-    _generate_regex(regex_path, license_id, root)
+    _generate_regex(regex_path, license_id, root, nf)
 
   meta['regex-dir'] = _dir
 
 
-def _generate_regex(dest: str, license_id: str, root: template.Node):
+def _generate_regex(dest: str, license_id: str, root: template.Node, nf: normalizer.Normalizer):
+  root = nf(root)
   with open(dest, 'w+') as f:
     f.write(str(root))
